2de902150d0474f4948d61625705e4bb69c751dd
AnupGhosh2605 <53530169+AnupGhosh2605@users.noreply.github.com>
2019-08-03 23:38:15 +0530

    Update Euler_question_6.java

--- Euler_question_6.java
+++ Euler_question_6.java
@@ -1,19 +1,31 @@
 
 public class Euler_question_6 {
-
+public static int sumOfTheSquares(int n, int sum)
+{
+	if(n==0)
+		return sum;
+	else
+	{
+		sum+=(n*n);
+		return sumOfTheSquares(n-1,sum);
+	}	
+}
+public static int SquareOfTheSum (int n,int sum)
+{
+	if(n==0)
+		return sum;
+	else
+	{
+		sum+=n;
+		return SquareOfTheSum(n-1,sum);
+	}
+}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- int sum=0;
-for(int i=1;i<=100;i++)
-{
-sum=sum+i;	
-}
-int ssum=0;
-for(int j=1;j<=100;j++)
-{
-	ssum=ssum+j*j;
-}
-System.out.println("Difference is "+((sum*sum)-ssum));
-	}
+ int sum=sumOfTheSquares(100,0);
+ int ssum=SquareOfTheSum(100,0);
+ System.out.println((ssum*ssum)-sum);
 
+	}
 }
+
