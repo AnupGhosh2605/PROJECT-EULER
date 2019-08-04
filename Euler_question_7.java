6b7a5a99c3edaa9174183a40b8841f035c6640b6
AnupGhosh2605 <53530169+AnupGhosh2605@users.noreply.github.com>
2019-08-04 18:08:11 +0530

    Update Euler_question_7.java

--- Euler_question_7.java
+++ Euler_question_7.java
@@ -1,16 +1,25 @@
 
 public class Euler_question_7 {
+	public static boolean isprime(int  a)
+	{
+		  int counter=0;
+		for(long i=1;i<=(long)Math.sqrt(a);i++) 
+		{
+			if(a%i==0)
+				counter++;
+			if(counter>1)
+				return false;
+		}
+			return true;
+	}
 	public static void main (String[]args) {
 		int c=0;
 		int n=0;
 	for(int i=2;i>0;i++)
 	{
-		for(int j=1;j<=i;j++)
-		{
-			if(i%j==0)
+		if(isprime(i)==true)
 			c++;
-		}
-	if(c==2)
+	if(c==1)
 	{
 		n++;
 	}
