e5cc168e7fa12bdd8ffd297e87686097d0bfe7be
AnupGhosh2605 <53530169+AnupGhosh2605@users.noreply.github.com>
2019-08-05 22:06:31 +0530

    Update Euler_question_6.java

--- Euler_question_6.java
+++ Euler_question_6.java
@@ -1,30 +1,13 @@
 
 public class Euler_question_6 {
-public static int sumOfTheSquares(int n, int sum)
-{
-	if(n==0)
-		return sum;
-	else
-	{
-		sum+=(n*n);
-		return sumOfTheSquares(n-1,sum);
-	}	
-}
-public static int SquareOfTheSum (int n,int sum)
-{
-	if(n==0)
-		return sum;
-	else
-	{
-		sum+=n;
-		return SquareOfTheSum(n-1,sum);
-	}
-}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
- int sum=sumOfTheSquares(100,0);
- int ssum=SquareOfTheSum(100,0);
- System.out.println((ssum*ssum)-sum);
+ int sumOfthesquares=(100*(100+1)*((2*100)+1))/6;
+int sumOfThenumber=(100*(100+1))/2;
+int difference=((sumOfThenumber*sumOfThenumber)-sumOfthesquares);
+System.out.println("Difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is 
+"+difference);
 
 	}
 }
