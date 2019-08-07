a58c1dfc24efa3241e92c33c78ca11d5d3382802
AnupGhosh2605 <53530169+AnupGhosh2605@users.noreply.github.com>
2019-08-07 09:02:05 +0530

    Update Euler_question_10.java

--- Euler_question_10.java
+++ Euler_question_10.java
@@ -1,9 +1,9 @@
 
 class Euler_question_10 {
-   public static boolean isprime(int  a)
+   public static boolean isprime(long  a)
 {
-	  int counter=0;
-	for(long i=1;i<=(long)Math.sqrt(a);i++) 
+	long counter=0;
+	for(long i=1;i<=(long)Math.sqrt(a);i=i+2) 
 	{
 		if(a%i==0)
 			counter++;
@@ -15,9 +15,9 @@ class Euler_question_10 {
  
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int x=2000000;
+		long x=2000000;
 		long sum=0;
-			for(int i=3;i<x;i=i+2)
+			for(long i=3;i<x;i=i+2)
 			{
 				if(isprime(i)==true)
 				{
